78c9b10b7c0388dda3328728aeed18d2665d0191
Tomi <developer@livestudio.eu>
2020-11-09 07:45:26 +0100

    update desktop silder

--- main.js
+++ main.js
@@ -7,91 +7,109 @@ var sliderHeight = 600;
 var sliderWidth = $(".slider-wrapper").width();
 
 if ($(window).width() > 767) {
-    desktopSlider();
+	desktopSlider();
 } else {
-    mobileSlider();
+	mobileSlider();
 }
 
 function desktopSlider() {
-    direction = "left";
+	direction = "left";
 
-    $(".slider.first").css("left", 0);
+	$(".slider.first").css("left", 0);
 
-    var timer = setInterval(() => { playAnimation("left", "left", ".slider", "right") }, sliderAutoplayTime);
+	var timer = setInterval(() => {
+		playAnimation("left", "left", ".slider", "right");
+	}, sliderAutoplayTime);
 
-    $(".slider").click(function () {
-        let elem = $(this);
-        let index = parseInt(elem.data("index"));
-        let position;
+	$(".slider").click(function () {
+		let elem = $(this);
+		let index = parseInt(elem.data("index"));
+		let position;
 
-        clearInterval(timer);
+		clearInterval(timer);
 
-        slideIndex = index;
+		slideIndex = index;
 
-        if (index == 0) return false;
+		if ($(`.slider:nth-of-type(${index + 2})`).hasClass("slide")) {
+			if (index == 3) {
+				return false;
+			} else {
+				$(".slider").each(function (pos, el) {
+					if ($(`.slider:nth-of-type(${pos + 2})`).hasClass("slide") && pos >= index) {
+						position = sliderWidth - (slideCount - (pos + 1)) * 50 + "px";
+						$(`.slider:nth-of-type(${pos + 2})`)
+							.stop()
+							.animate({ left: position }, sliderSpeed);
+						$(`.slider:nth-of-type(${pos + 2})`).toggleClass("active slide");
+					}
+				});
+			}
+		} else {
+			if (index == 1) {
+				position = index * 50 + "px";
+				elem.stop().animate({ left: position }, 500);
+				elem.toggleClass("slide");
+			} else {
+				$(".slider").each(function (pos, el) {
+					if (pos != 0 && !$(el).hasClass("slide") && pos <= index) {
+						position = pos * 50 + "px";
+						$(el).stop().animate({ left: position }, sliderSpeed);
+						$(el).toggleClass("slide");
+					}
+				});
+			}
+		}
 
-        if (elem.hasClass("slide")) {
-        $(".slider").each(function (pos, el) {
-            if ($(el).hasClass("slide") && pos >= index) {
-                position = (sliderWidth - ((slideCount - pos + 1) * 50)) + "px";
-                $(el).stop().animate({ left: position }, sliderSpeed);
-                $(el).toggleClass("slide");
-            }
-        });
-        } else {
-            $(".slider").each(function (pos, el) {
-                if (pos != 0 && !$(el).hasClass("slide") && pos <= index) {
-                    position = (pos * 50) + "px";
-                    $(el).stop().animate({ left: position }, sliderSpeed);
-                    $(el).toggleClass("slide");
-                }
-            });
-        }
-
-        checkIndex("left", "right");
-        timer = setInterval(() => { playAnimation("left", "left", ".slider", "right") }, sliderAutoplayTime);
-    });
+		checkIndex("left", "right");
+		timer = setInterval(() => {
+			playAnimation("left", "left", ".slider", "right");
+		}, sliderAutoplayTime);
+	});
 }
 
 function mobileSlider() {
-    direction = "top";
+	direction = "top";
 
-    $(".m-slider.first").css("top", 0);
+	$(".m-slider.first").css("top", 0);
 
-    var timer = setInterval(() => { playAnimation("top", "top", ".m-slider", "bottom") }, sliderAutoplayTime);
+	var timer = setInterval(() => {
+		playAnimation("top", "top", ".m-slider", "bottom");
+	}, sliderAutoplayTime);
 
-    $(".m-slider").click(function () {
-        let elem = $(this);
-        let index = parseInt(elem.data("index"));
-        let position;
+	$(".m-slider").click(function () {
+		let elem = $(this);
+		let index = parseInt(elem.data("index"));
+		let position;
 
-        clearInterval(timer);
+		clearInterval(timer);
 
-        slideIndex = index;
+		slideIndex = index;
 
-        if (index == 0) return false;
+		if (index == 0) return false;
 
-        if (elem.hasClass("slide")) {
-        $(".m-slider").each(function (pos, el) {
-            if ($(el).hasClass("slide") && pos >= index) {
-                position = (sliderHeight - ((slideCount - pos + 1) * 50)) + "px";
-                $(el).stop().animate({ top: position }, sliderSpeed);
-                $(el).toggleClass("slide");
-            }
-        });
-        } else {
-            $(".m-slider").each(function (pos, el) {
-                if (pos != 0 && !$(el).hasClass("slide") && pos <= index) {
-                    position = (pos * 50) + "px";
-                    $(el).stop().animate({ top: position }, sliderSpeed);
-                    $(el).toggleClass("slide");
-                }
-            });
-        }
+		if (elem.hasClass("slide")) {
+			$(".m-slider").each(function (pos, el) {
+				if ($(el).hasClass("slide") && pos >= index) {
+					position = sliderHeight - (slideCount - pos + 1) * 50 + "px";
+					$(el).stop().animate({ top: position }, sliderSpeed);
+					$(el).toggleClass("slide");
+				}
+			});
+		} else {
+			$(".m-slider").each(function (pos, el) {
+				if (pos != 0 && !$(el).hasClass("slide") && pos <= index) {
+					position = pos * 50 + "px";
+					$(el).stop().animate({ top: position }, sliderSpeed);
+					$(el).toggleClass("slide");
+				}
+			});
+		}
 
-        checkIndex("top", "bottom");
-        timer = setInterval(() => { playAnimation("top", "top", ".m-slider", "bottom") }, sliderAutoplayTime);
-    });
+		checkIndex("top", "bottom");
+		timer = setInterval(() => {
+			playAnimation("top", "top", ".m-slider", "bottom");
+		}, sliderAutoplayTime);
+	});
 }
 
 /**
@@ -104,25 +122,27 @@ function mobileSlider() {
  * return void
  */
 function playAnimation(sliderDirection, animateDirection, sliderClass, secondaryDirection) {
-    var position;
-    if (direction === sliderDirection) {
-        position = (slideIndex * 50) + "px";
-        $(`${sliderClass}:nth(${slideIndex})`).addClass("slide");
-    } else {
-        if(sliderDirection == "left") {
-            position = (sliderWidth - ((slideCount - (slideIndex - 1)) * 50)) + "px";
-        } else {
-            position = (sliderHeight - ((slideCount - (slideIndex - 1)) * 50)) + "px";
-        }
-        $(`${sliderClass}:nth(${slideIndex})`).removeClass("slide");
-    }
+	var position;
+	if (direction === sliderDirection) {
+		position = slideIndex * 50 + "px";
+		$(`${sliderClass}:nth(${slideIndex})`).addClass("slide");
+	} else {
+		if (sliderDirection == "left") {
+			position = sliderWidth - (slideCount - slideIndex) * 50 + "px";
+		} else {
+			position = sliderHeight - (slideCount - (slideIndex - 1)) * 50 + "px";
+		}
+		$(`${sliderClass}:nth(${slideIndex})`).removeClass("slide");
+	}
 
-    let animationParams = {};
-    animationParams[animateDirection] = position;
+	let animationParams = {};
+	animationParams[animateDirection] = position;
 
-    $(`${sliderClass}:nth(${slideIndex})`).stop().animate(animationParams, sliderSpeed, function () {
-        checkIndex(animateDirection, secondaryDirection);
-    });
+	$(`${sliderClass}:nth(${slideIndex})`)
+		.stop()
+		.animate(animationParams, sliderSpeed, function () {
+			checkIndex(animateDirection, secondaryDirection);
+		});
 }
 
 /**
@@ -134,13 +154,13 @@ function playAnimation(sliderDirection, animateDirection, sliderClass, secondary
  * return void
  */
 function checkIndex(direction1, direction2) {
-    if (slideIndex == 1 && direction == direction2) {
-        direction = direction1;
-    } else if (slideIndex >= 1 && direction == direction1 && slideIndex < slideCount) {
-        slideIndex++;
-    } else if ((slideIndex == slideCount || slideIndex < slideCount) && direction == direction2) {
-        slideIndex--;
-    } else if (slideIndex == slideCount) {
-        direction = direction2;
-    }
+	if (slideIndex == 1 && direction == direction2) {
+		direction = direction1;
+	} else if (slideIndex >= 1 && direction == direction1 && slideIndex < slideCount) {
+		slideIndex++;
+	} else if ((slideIndex == slideCount || slideIndex < slideCount) && direction == direction2) {
+		slideIndex--;
+	} else if (slideIndex == slideCount) {
+		direction = direction2;
+	}
 }
